f1b90b1182f145a92835a3005d3507a9db52ab77
3eboo <tech@seerreal.com>
2022-01-01 05:07:48 +0100

    faust app consumer and counter

--- event_stream.py
+++ event_stream.py
@@ -1,26 +1,43 @@
 import faust
+import json
+from kafka import KafkaProducer
 
 
-# model for the frames
-class Frame(faust.Record, serializer='json'):
-    uuid: str
+class Frame(faust.Record, validation=True, serializer='json'):
+    uid: str
     ts: int
 
 
-# application definition
-app = faust.App('my-app', broker='kafka://localhost:9092')
-# input stream
-frames_kafka_topic = app.topic('frames', value_type=Frame)
-# counts table definition
-user_count_by_uuid = app.Table('frame_count', default=int)
+app = faust.App("log-frames-streamer", broker="kafka://{}".format("localhost"))
+frames_topic = app.topic("frames", value_type=str, value_serializer=json, partitions=8)
+uid_topic = app.topic("uid", key_type=str, value_type=Frame)
+uid_counts = app.Table("uid_counts", default=int, key_type=str, value_type=int)
 
 
-@app.agent(frames_kafka_topic)
-async def process(frames: faust.Stream[Frame]) -> None:
-    async for frame in frames.group_by(Frame.uuid):
-        user_count_by_uuid[frame.uuid] += 1
-        print('hi')
+@app.agent(frames_topic)
+async def process(events: str):
+    async for event in events:
+        for frame in json.loads(event):
+            await process_event.send(
+                key=frame['uid'],
+                value=Frame(frame['uid'], frame['ts']))
+        else:
+            print(f'event: {event}  events:{events}')
+        # frames = json.loads(events)
+        # for frame in frames:
 
+
+
+@app.agent(uid_topic)
+async def process_event(frames: faust.Stream[Frame]) -> None:
+    async for frame in frames.group_by(Frame.uid):
+        uid_counts[frame.uid] += 1
+        print(f'frame {frame.uid} has count{uid_counts[frame.uid]}')
+
+
+@app.timer(60.0)
+async def my_periodic_task():
+    print('print counts for each uid')
 
 if __name__ == "__main__":
     app.main()
